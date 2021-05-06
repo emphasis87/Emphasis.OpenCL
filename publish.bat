2f8710aa03cb7ceb6eefd1db3c35159d983d1923
lubos_hemala <lubos.hemala@gmail.com>
2021-05-06 23:51:24 +0200

    script

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 rmdir /s /q "pack"
-dotnet clean "src\Emphasis.ScreenCapture.sln"
-dotnet pack "src\Emphasis.ScreenCapture.sln" -c Release --include-source --include-symbols -o pack
+dotnet clean "src\Emphasis.OpenCL.sln"
+dotnet pack "src\Emphasis.OpenCL.sln" -c Release --include-source --include-symbols -o pack
 dotnet nuget push "pack\*.nupkg" -s https://api.nuget.org/v3/index.json --skip-duplicate
