81a50a92b7eeb517ffeccec334d0fada58a5a2da
lubos_hemala <lubos.hemala@gmail.com>
2021-05-23 19:27:02 +0200

    memory pool

--- src/Emphasis.OpenCL/OclMemoryPool.cs
+++ src/Emphasis.OpenCL/OclMemoryPool.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
 using Silk.NET.OpenCL;
 using static Emphasis.OpenCL.OclHelper;
 
@@ -16,7 +18,16 @@ namespace Emphasis.OpenCL
 	{
 		public static OclMemoryPool Default { get; } = new OclMemoryPool();
 
-		private readonly ConcurrentDictionary<(nint contextId, int memFlags), SortedSet<OclBuffer>> _buffers = new();
+		private readonly ConcurrentDictionary<(nint contextId, int memoryFlags), ContextMemoryFlagsBucket> _contextBuckets = new();
+		private readonly Timer _gcTimer;
+
+		public OclMemoryPool()
+		{
+			_gcTimer = new Timer();
+			_gcTimer.Elapsed += (sender, args) => TrimExcess();
+			_gcTimer.Interval = TimeSpan.FromSeconds(1).Milliseconds;
+			_gcTimer.Start();
+		}
 
 		public nint RentBuffer<T>(nint contextId, int memoryFlags, int minSize) where T : unmanaged
 		{
@@ -25,27 +36,72 @@ namespace Emphasis.OpenCL
 			if ((memoryFlags & (int) CLEnum.MemCopyHostPtr) != 0)
 				throw new Exception("Unable to use CL_MEM_COPY_HOST_PTR as memory flags within OpenCL buffer memory pool.");
 			
-			if (!_buffers.TryGetValue((contextId, memoryFlags), out var buffers))
-				buffers = _buffers.GetOrAdd((contextId, memoryFlags), new SortedSet<OclBuffer>());
-			
-			lock (buffers)
+			if (!_contextBuckets.TryGetValue((contextId, memoryFlags), out var contextBucket))
 			{
-				var range = buffers.GetViewBetween(new OclBuffer(default, minSize), new OclBuffer(default, int.MaxValue));
-				if (!range.TryGetValue(range.Min, out var buffer))
-				{
-					range.Remove(buffer);
-					return buffer.BufferId;
-				}
+				contextBucket = _contextBuckets.GetOrAdd((contextId, memoryFlags), new ContextMemoryFlagsBucket(contextId, memoryFlags));
 			}
 
-			var bufferId = CreateBuffer<T>(contextId, minSize, memoryFlags);
-			return bufferId;
+			lock (contextBucket)
+			{
+				if (TryRentBuffer(contextBucket, out var rentedBufferId))
+					return rentedBufferId;
+			}
+			
+			var createdBufferId = CreateBuffer<T>(contextId, minSize, memoryFlags);
+			return createdBufferId;
+		}
+
+		private bool TryRentBuffer(ContextMemoryFlagsBucket contextBucket, out nint bufferId)
+		{
+
+			var bufferBuckets = contextBucket.BufferBuckets;
+			var count = bufferBuckets.Count;
+			if (count == 0)
+			{
+				bufferId = default;
+				return false;
+			}
+
+			var n0 = 0;
+			var n1 = bufferBuckets.Count - 1;
+			while (n0 != n1)
+			{
+
+			}
+
+			bufferId = default;
+			return false;
 		}
 
 		public void ReturnBuffer(nint bufferId)
 		{
 			throw new NotImplementedException();
 		}
+
+		public void TrimExcess()
+		{
+			
+		}
+
+		internal class ContextMemoryFlagsBucket
+		{
+			public nint ContextId { get; }
+			public int MemoryFlags { get; }
+
+			public readonly SortedList<int, BufferBucket> BufferBuckets = new();
+
+			public ContextMemoryFlagsBucket(nint contextId, int memoryFlags)
+			{
+				ContextId = contextId;
+				MemoryFlags = memoryFlags;
+			}
+		}
+
+		internal class BufferBucket
+		{
+			
+		}
+
 
 		internal readonly struct OclBuffer : IComparable<OclBuffer>
 		{
