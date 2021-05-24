8b47b03947222c41d22ed8604fb2603ca983678d
Luboš Hemala <lubos.hemala@gmail.com>
2021-05-24 05:42:40 +0200

    tests

---
+++ src/Emphasis.OpenCL.Tests/OclMemoryPoolTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Emphasis.OpenCL.Tests
+{
+	public class OclMemoryPoolTests
+	{
+		[Test]
+		public void Test()
+		{
+			var list = new SortedList<(int,long), string>();
+			list.Add((1, DateTime.Now.Ticks), "a");
+			list.Add((1, DateTime.Now.Ticks), "b");
+
+			foreach (var v in list)
+			{
+				Console.WriteLine($"{v.Key} = {v.Value}");
+			}
+		}
+	}
+}
